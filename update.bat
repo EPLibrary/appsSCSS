b3cd99c3dcf4a40d0c4d2803ffc5b2fe43a12e99
JD Lien <jd@jdlien.com>
2020-06-08 20:27:24 -0600

    Support for filterForms

--- update.bat
+++ update.bat
@@ -1,6 +1,3 @@
 echo Updating CSS Files to production...
-copy .\print.css W:\CSS
-copy .\appsDark.css W:\CSS
-copy .\appsDarkForced.css W:\CSS
-copy .\apps.css.map W:\CSS
-copy .\apps.css W:\CSS
+copy .\*.*css W:\CSS
+copy .\*.map W:\CSS
